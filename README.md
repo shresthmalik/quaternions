0926ca4db72d1580c6bc3f46e4faace404783ec7
shresthmalik <shresthmalik.official@gmail.com>
2025-03-22 11:09:52 +0530

    calculations now working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # quaternions
+
+Not meant for public access
+A Simple calculation tool to test my own understanding of Quaternions
--- quaternion.cpp
+++ quaternion.cpp
@@ -11,10 +11,10 @@ struct vec3D{
     }
 };
 
-struct quaternions{
+struct quaternion{
     float a=0, b=0, c=0, d=0;
-    quaternions(){}
-    quaternions(float a, float b, float c, float d){
+    quaternion(){}
+    quaternion(float a, float b, float c, float d){
         this->a = a;
         this->b = b;
         this->c = c;
@@ -22,25 +22,32 @@ struct quaternions{
     }
 };
 
-quaternions conjugate(quaternions q){
-    quaternions q_;
+void printQuar(quaternion q, string s);
+void printvec3D(vec3D p, string s);
+
+quaternion conjugate(quaternion q){
+    quaternion q_;
     q_.a = q.a;
-    q_.b = - q.b;
-    q_.c = - q.c;
-    q_.d = - q.d;
+    q_.b = 0 - q.b;
+    q_.c = 0 - q.c;
+    q_.d = 0 - q.d;
+    return q_;
 }
 
-quaternions quartize(vec3D axis, float theta){
-    quaternions q;
+quaternion quartize(vec3D axis, float theta){
+    quaternion q;
     q.a = cos(theta/2);
+    // cout<<"theta = "<<theta<<endl;
+    // cout<<"cos(theta/2) = "<<cos(theta/2)<<endl;
     
     q.b = sin(theta/2) * axis.x;
     q.c = sin(theta/2) * axis.y;
     q.d = sin(theta/2) * axis.z;
+    return q;
 }
 
-quaternions mul(quaternions x, quaternions y){
-    quaternions ans(0,0,0,0);
+quaternion mul(quaternion x, quaternion y){
+    quaternion ans(0,0,0,0);
     ans.a = x.a * y.a  -  x.b * y.b  -  x.c * y.c  -  x.d * y.d  ;
     ans.b = x.a * y.b  +  x.b * y.a  +  x.c * y.d  -  x.d * y.c  ;
     ans.c = x.a * y.c  +  x.c * y.a  +  x.d * y.b  -  x.b * y.d  ;
@@ -48,36 +55,40 @@ quaternions mul(quaternions x, quaternions y){
     return ans;
 }
 
-quaternions rotate(vec3D pos, vec3D axis, float theta){
+quaternion rotate(vec3D pos, vec3D axis, float theta){
     
     //normalize axis
     float scl = sqrt(pow(axis.x,2) + pow(axis.y,2) + pow(axis.z,2));
     axis.x /= scl; axis.y /= scl; axis.z /= scl;
     
-    quaternions q = quartize(axis, theta);
-    quaternions q_ =conjugate(q) ;
-    quaternions v = quartize(pos, 1.57); //default theta = 90
+    quaternion q = quartize(axis, theta);
+    quaternion q_ =conjugate(q) ;
+    quaternion v = quartize(pos, M_PI); //default theta = 180
 
-    quaternions rotated_v = mul(mul(q,v),q_) ;
+    printQuar(q,"q");
+    printQuar(q_,"q_");
+    printQuar(v,"v");
+
+    quaternion rotated_v = mul(mul(q,v),q_) ;
     return rotated_v ;
 }
 
-void printvec3D(vec3D p){
-    cout<<p.x<<","<<p.y<<","<<p.z<<endl;
+void printvec3D(vec3D p, string s){
+    cout<<s<<" = "; 
+    cout<<p.x<<", "<<p.y<<", "<<p.z<<endl;
 }
 
-void printQuar(quaternions q){
-    cout<<q.a<<","<<q.b<<","<<q.c<<","<<q.d<<endl;
+void printQuar(quaternion q, string s){
+    cout<<s<<" = "; 
+    cout<<q.a<<", "<<q.b<<", "<<q.c<<", "<<q.d<<endl;
 }
-
 
 int main(){
 
     vec3D pos(1,2,3);
     vec3D axis(1,0,0);
 
-    quaternions rotated_q = rotate(pos,axis,1.57);
-
-    printQuar(rotated_q);
+    quaternion rotated_q = rotate(pos,axis,M_PI/2);
+    printQuar(rotated_q, "rotated q");
 
 }
